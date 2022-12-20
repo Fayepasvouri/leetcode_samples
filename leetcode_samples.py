9e5e6efd12f8d8487052c8b0a4409198dd536af4
Faye Pasvouri <45490922+Fayepasvouri@users.noreply.github.com>
2022-12-20 21:31:45 +0000

    Update leetcode_samples.py

--- leetcode_samples.py
+++ leetcode_samples.py
@@ -296,3 +296,34 @@ def partition(array):
         return array
 
 partition([8, 7, 2, 1, 0, 9, 6])
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def isSameTree(self, p, q) -> bool:
+        self.p = p 
+        self.q = q 
+
+        if p is None and q is None:
+            return True
+
+        if p is not None and q is not None:
+            return (p.val == q.val) and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        return False 
+
+tree = TreeNode(1)
+sec_tree = TreeNode(1)
+tree.left = TreeNode(2)
+tree.right = TreeNode(3)
+sec_tree.left = TreeNode(2)
+sec_tree.right = TreeNode(4)
+
+if __name__ == "__main__":
+    sol = Solution()
+    if sol.isSameTree(tree, sec_tree):
+        print("Both trees are identical")
+    else:
+        print("Trees are not identical")
