b460ebc540e7226e9924f0747a981190196e1587
Faye Pasvouri <45490922+Fayepasvouri@users.noreply.github.com>
2023-01-15 18:10:25 +0000

    Update leetcode_samples.py

--- leetcode_samples.py
+++ leetcode_samples.py
@@ -897,4 +897,37 @@ if __name__ == "__main__":
         print(False)
 
 
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def sumOfLeftLeaves(self, root) -> int:
+        
+        self.root = root
+
+        if root is None or [root.val] == 0:
+            return 0 
+        
+        res = 0 
+        if root.left:
+            if not root.left.left and not root.left.right:  # left leaf
+                res += root.left.val
+            else:
+                res += self.sumOfLeftLeaves(root.left)
+
+        if root.right:
+            res += self.sumOfLeftLeaves(root.right)
+        return res
+        
+
+tree = TreeNode(3)
+tree.left = TreeNode(9)
+tree.right = TreeNode(20)
+tree.right.left = TreeNode(15)
+tree.right.right = TreeNode(7)
+sol = Solution()
+sol.sumOfLeftLeaves(tree)
 
