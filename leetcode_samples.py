26e771b42c865f3ac701a4d1e5d1e7bad53ebfab
Faye Pasvouri <45490922+Fayepasvouri@users.noreply.github.com>
2024-10-31 15:30:38 +0000

    Update leetcode_samples.py

--- leetcode_samples.py
+++ leetcode_samples.py
@@ -988,3 +988,27 @@ class IntegerDivision:
         
 result = IntegerDivision([10, 3, 7, -3])
 result.divide_integers()
+
+# F(0) = 0, F(1) = 1
+# F(n) = F(n - 1) + F(n - 2), for n > 1.
+
+# Dynamic Programming
+
+class Fibonacci:
+
+    def __init__(self, n):
+        self.n = n
+
+    def fibonacci(self, n) -> int:
+        self.n = n
+        if n == 0:
+            return 0 
+        elif n == 1:
+            return 1 
+        
+        return self.fibonacci(n - 1) + self.fibonacci(n - 2)
+
+
+result = Fibonacci(3)
+print(result.fibonacci(3))
+
